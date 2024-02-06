982aec8feec4c3ae7598e7f09338c32b5a5a8d42
JoseRubicco21 <a23joseRF@iessanclemnte.net>
2024-02-06 12:25:26 +0100

    finished the excercise

--- Python/RubiccoJose_Menu.py
+++ Python/RubiccoJose_Menu.py
@@ -1,4 +1,4 @@
-import os, webbrowser, time
+import os, webbrowser, time, stat
 
 menu_state=-1
 
@@ -7,9 +7,10 @@ def display_menu():
     print("[1] Aprender a programar en C++")
     print("[2] Destruir el sistema Operativo llenando los I-Nodos ")
     print("[3] Instalar / Iniciar pong (Debe de ser root)")
-    print("[4] 'Hola Mundo!' ")
-    print("[5] Calcular el numero N de la secuencia Fibonacci")
-    print("[6] Salir")
+    print("[4] Correr Pong")
+    print("[5] 'Hola Mundo!' ")
+    print("[6] Calcular el numero N de la secuencia Fibonacci")
+    print("[7] Salir")
 
 
 def clear():
@@ -42,12 +43,24 @@ def reset_menu_state(menu_state):
     menu_state=-1
 
 
-def installPong():
+def install_pong():
     ## This could be done with python libraries such as python-git. For sake of simplicity and 
     ## and not using virutal environments we'll just simulate the bash script using os.system when
     ## needed.
-    
-    os.system("""echo "test" """)
+
+    ## WARNING: UNTESTED
+    installation_dir = input("Introduzca la ruta donde instalara pong ")
+    os.mkdir(installation_dir)
+    os.chdir(installation_dir)
+    os.system("apt -y install default-jre")
+    os.system("apt -y install default-jdk")
+    os.system("git clone https://github.com/Jnv821/Pong.git")
+    os.chdir(os.getcwd()+"/Pong")
+    os.system("chmod +x gradlew")
+    os.system("./gradlew desktop:dist")
+
+def run_pong(pong_location):
+    os.system(f"java -jar {pong_location}/desktop/build/libs/desktop-1.0.jar ")
 
 def run_menu():
     display_menu()
@@ -64,22 +77,26 @@ def run_menu():
             time.sleep(1)
             clear()
         case 3:
-            installPong()
-            print("No.")
+            install_pong()
             reset_menu_state(menu_state=menu_state)
             time.sleep(1)
             clear()
         case 4:
+            run_pong(input("Introduce La localizacion de Pong "))
+            reset_menu_state(menu_state=menu_state)
+            time.sleep(1)
+            clear
+        case 5:
             hola_mundo()
             reset_menu_state(menu_state=menu_state)
             time.sleep(1)
             clear()
-        case 5:
+        case 6:
             print("El numero es " + str(calcular_fibonacci(int(input("Introduzca el numero de la secuencia fibonacci que desea calcular ")))))
             reset_menu_state(menu_state=menu_state)
             time.sleep(1)
             clear()
-        case 6:
+        case 7:
             clear()
             print("Saliendo del programa")
             menu_state=0
